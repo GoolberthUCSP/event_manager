ec3353e24f01bde99d6efea2d41e37489a0be155
Fredy Goolberth Quispe Neira <goolberth.neira@gmail.com>
2023-04-06 15:00:46 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import render_template, request, redirect
+
+app = Flask(__name__)
+
+class User:
+    def __init__(self, _email, _password, _type):
+        self.email = _email
+        self.password = _password
+        self.type = _type
+
+users = []
+users.append(User("admin_test@gmail.com", "passwordAdmin", 1))
+users.append(User("encargado_test@gmail.com", "passwordEncargado", 2))
+users.append(User("colaborador_test@gmail.com", "passwordColaborador", 3))
+
+@app.route("/", methods =["GET", "POST"])
+def home():
+    if request.method == "POST":
+        for user in users:
+            if user.email == request.form.get("email") and user.password == request.form.get("password"):
+                if (user.type == 1):
+                    return redirect("/admin")
+                elif (user.type == 2):
+                    return redirect("/encargado")
+                else:
+                    return redirect("/colaborador")
+                
+    return render_template("login.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/encargado")
+def encargado():
+    return render_template("encargado.html")
+
+@app.route("/colaborador")
+def colaborador():
+    return render_template("colaborador.html")
+
+if (__name__ == "__main__"):
+    app.run()
+
+
